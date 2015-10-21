c0874181e71da6e3f5c99b8f039906da99a9a8df
GogoK <georgiakanchev@gmail.com>
2015-10-21 19:28:55 +0300

    Exam 2

--- 01.18October2015/01.TheBetterMusicProducer/TheBetterMusicProducer.cs
+++ 01.18October2015/01.TheBetterMusicProducer/TheBetterMusicProducer.cs
@@ -5,24 +5,24 @@ class TheBetterMusicProducer
     static void Main()
     {
         decimal europe = decimal.Parse(Console.ReadLine());
-        decimal euAlbumPrice = decimal.Parse(Console.ReadLine());   //  price in euro   -   1 euro is 1.94lv.
-        
+        decimal euAlbumPrice = decimal.Parse(Console.ReadLine());
+
         decimal northAmerica = decimal.Parse(Console.ReadLine());
-        decimal naAlbumPrice = decimal.Parse(Console.ReadLine());   //  dollars         -   1 dollar is 1.72lv.
-        
+        decimal naAlbumPrice = decimal.Parse(Console.ReadLine());
+
         decimal southAmerica = decimal.Parse(Console.ReadLine());
-        decimal saAlbumPrice = decimal.Parse(Console.ReadLine());   // pesos            -   332.74 pesos are 1lv.
-        
+        decimal saAlbumPrice = decimal.Parse(Console.ReadLine());
+
         decimal numberOfConcerts = decimal.Parse(Console.ReadLine());
-        decimal singleConcert = decimal.Parse(Console.ReadLine());   // euro            -   1 euro is 1.94lv.
-        
+        decimal singleConcert = decimal.Parse(Console.ReadLine());
+
         europe *= euAlbumPrice * 1.94m;
         northAmerica *= naAlbumPrice * 1.72m;
         southAmerica *= saAlbumPrice / 332.74m;
 
         decimal sumAlbums = europe + northAmerica + southAmerica;
         decimal producerCommission = sumAlbums * 0.35m;
-        decimal totalAlbums = (sumAlbums - producerCommission) * 0.80m; // -20% other taxes
+        decimal totalAlbums = (sumAlbums - producerCommission) * 0.80m;
 
         decimal totalConcerts = numberOfConcerts * singleConcert * 1.94m;
         if (totalConcerts > 100000)
@@ -32,17 +32,11 @@ class TheBetterMusicProducer
 
         if (totalConcerts < totalAlbums)
         {
-            /*If the total profit from all album is more than from all concerts print on the console:
-                “Let’s record some songs! They’ll bring us {incomes from albums}lv.”
-             */
-            Console.WriteLine("Let’s record some songs! They’ll bring us {0:F}lv.", totalAlbums);
+            Console.WriteLine("Let's record some songs! They'll bring us {0:F}lv.", totalAlbums);
         }
         else
         {
-            /*
-            If it is equal to or the profit from the concerts is greater:
-                “On the road again! We’ll see the world and earn {profit from concerts}lv.”*/
-            Console.WriteLine("On the road again! We’ll see the world and earn {0:F}lv.", totalConcerts);
+            Console.WriteLine("On the road again! We'll see the world and earn {0:F}lv.", totalConcerts);
         }
     }
 }
---
+++ 01.18October2015/02.GrandTheftExamo/GrandTheftExamo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class GrandTheftExamo
+{
+    static void Main()
+    {
+        int numberOfEscapeAttempts = int.Parse(Console.ReadLine());
+        
+        long slappedThieves = 0;
+        long escapeThieves = 0;
+        long allBeerBottles = 0;
+
+        //if (numberOfEscapeAttempts > 50)
+        //{
+        //    numberOfEscapeAttempts = 50;
+        //}
+        for (int i = 0; i < numberOfEscapeAttempts; i++)
+        {
+            int amountOfThieves = int.Parse(Console.ReadLine());
+            int amountOfBeers = int.Parse(Console.ReadLine());
+            if (amountOfThieves > 5)
+            {
+                slappedThieves += 5;
+                escapeThieves += amountOfThieves - 5;
+            }
+            else
+            {
+                slappedThieves += amountOfThieves;
+            }
+            allBeerBottles += amountOfBeers;
+        }
+
+        Console.WriteLine("{0} thieves slapped.", slappedThieves);
+        Console.WriteLine("{0} thieves escaped.", escapeThieves);
+        Console.WriteLine("{0} packs, {1} bottles.", (allBeerBottles / 6), (allBeerBottles % 6));
+    }
+}
